f20f4124887e83c9ee2224250bd8225b8243b91d
Tariq Rashid <tariqall@hotmail.com>
2024-07-21 00:08:03 +0500

    changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='registration', view=views.registration, name='register'),
+    path(route='register', view=views.registration, name='register'),
     
     # path for login
     path(route='login', view=views.login_user, name='login'),
